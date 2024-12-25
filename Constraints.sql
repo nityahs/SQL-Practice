d2aeafa8f01cd818eb8768aa45bbbb5065c432df
Nitya Shah <148626044+nityahs@users.noreply.github.com>
2024-12-26 01:17:07 +0530

    understanding few more constraints

--- Constraints.sql
+++ Constraints.sql
@@ -42,3 +42,65 @@ alter table person
 drop  primary key;
 desc person;
 alter table person add primary key(id);
+
+#Constraints 2
+/*
+check constraint
+foreign key
+default constraint
+unique constraint
+*/
+create table person1(
+id integer,
+first_name varchar(25) not null,
+last_name varchar(25) not null
+);
+alter table person1 modify id integer not null;
+alter table person1 modify first_name varchar(10);
+desc person1;
+alter table person1 modify id varchar(10) not null;
+alter table person1 add constraint primary key(id,first_name);
+alter table person1 drop primary key;
+alter table person1 modify id int primary key;
+alter table person1 add salary int;
+
+-- foreign key
+create table department(
+id int not null,
+department_name varchar(25) not null,
+dept_id int not null,
+primary key(dept_id),
+constraint fk_person1dept foreign key(id) references person1(id)
+);
+DESC department;
+alter table department drop foreign key fk_person1dept; #drops specified foreign key
+alter table department add foreign key fk_person1dept(id) references person1(id);#alters to add foreing key
+drop table person,department;
+-- check constraint
+create table person(
+id int not null,
+first_name varchar(25),
+last_name varchar(25),
+salary integer not null,
+primary key(id),
+check(salary<50000)); #works as a constraint in regards to value
+desc person;
+insert into person(id, first_name, last_name, salary) values (1,'Nitya', 'Shah',60000); #this fails the check where salary>50000
+insert into person(id, first_name, last_name, salary) values (1,'Nitya', 'Shah',20000); #this fails the check where salary<50000
+alter table person drop check person_chk_1; #we got the name of constraint from below statement
+show create table person; #helps us identify the name of constraint set by default while making a constraint
+alter table person add constraint check(salary<50000);
+select * from person;
+drop table person;
+-- default constraint
+create table person(
+id int not null,
+first_name varchar(25),
+city varchar(25) default 'Bangalore' #this sets default value for the given column
+);
+insert into person(id,first_name) values(1,'Nitya');
+insert into person values(2,'Shah','Mumbai');
+select * from person;
+alter table person alter city drop default; #used to drop default
+alter table person alter city set default 'Bangalore'; #alter to change default
+
